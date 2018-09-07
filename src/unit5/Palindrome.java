a2dd6405b159d3014b0eaab33d32b53c441b7c24
武扬 <956826374@qq.com>
2018-09-07 09:21:16 +0800

    判断是否为回文字符串

---
+++ src/unit5/Palindrome.java
@@ -0,0 +1,22 @@
+/*
+输入一个字符串判断该字符是否为回文字符串
+ */
+package unit5;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] agrs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入一个字符串：");
+        String s = scanner.next();
+        for(int i=0, l=s.length()-1; i<l; i++,l--){
+            if(s.charAt(i) != s.charAt(l)){
+                System.out.printf("%s不是回文字符串",s);
+                System.exit(0);
+            }
+        }
+        System.out.printf("%s是回文字符串",s);
+        System.exit(0);
+    }
+}
