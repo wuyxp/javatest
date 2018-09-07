0b5baaaf5d860d7122ced157388c963e57a7614d
武扬 <956826374@qq.com>
2018-09-07 10:00:20 +0800

    输出金字塔

--- src/unit5/Palindrome.java
+++ src/unit5/Palindrome.java
@@ -20,3 +20,6 @@ public class Palindrome {
         System.exit(0);
     }
 }
+
+
+
---
+++ src/unit5/Pyramid.java
@@ -0,0 +1,33 @@
+/*
+输入一个数字，然后打印出对应大小的金字塔
+比如输入
+    4
+那么输出
+            1
+          2 1 2
+        3 2 1 2 3
+      4 3 2 1 2 3 4
+ */
+package unit5;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] agrs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入一个金字塔的高度：");
+        int height = scanner.nextInt();
+        for(int i=1; i<=height;i++){
+            for(int j=0;j<(height-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>0;j--){
+                System.out.printf("%d ",j);
+            }
+            for(int j=1;j<i;j++){
+                System.out.printf("%d ",j+1);
+            }
+            System.out.print("\n");
+        }
+    }
+}
