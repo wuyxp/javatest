b7cf9a09c35150f92310e8e02dbf86e0c2bc1015
武扬 <956826374@qq.com>
2018-09-09 11:08:18 +0800

    生成随机字符串

---
+++ src/unit6/RandomCharacter.java
@@ -0,0 +1,27 @@
+/*
+随机生成指定范围内的一个字符
+ */
+package unit6;
+
+public class RandomCharacter {
+    public static char getRandomCharacter(char c1, char c2) {
+        return (char) (c1 + Math.random() * (c2 - c1 + 1));
+    }
+    public static char getRandomChar(String s) {
+        char r = ' ';
+        switch (s) {
+            case "LowerCase":
+                r = getRandomCharacter('a', 'z');
+                break;
+            case "UpperCase":
+                r = getRandomCharacter('A', 'Z');
+                break;
+            case "Digit":
+                r = getRandomCharacter('0', '9');
+                break;
+            default:
+                r = getRandomCharacter('\u0000', '\uFFFF');
+        }
+        return r;
+    }
+}
