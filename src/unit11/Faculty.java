1023684054be9e623ff33f43c90c744f582fea94
武扬 <956826374@qq.com>
2018-09-13 21:31:18 +0800

    构造方法链，方法链调用系列

---
+++ src/unit11/Faculty.java
@@ -0,0 +1,32 @@
+package unit11;
+
+/*
+构造方法链
+ */
+public class Faculty extends Employee {
+    public static void main(String[] args) {
+        new Faculty();
+    }
+
+    public Faculty() {
+        System.out.println("444444444444");
+    }
+
+}
+
+class Employee extends Person {
+    public Employee(String s) {
+        System.out.println(s);
+    }
+
+    public Employee() {
+        this("222222222");
+        System.out.println("3333333333");
+    }
+}
+
+class Person {
+    public Person() {
+        System.out.println("11111111");
+    }
+}
