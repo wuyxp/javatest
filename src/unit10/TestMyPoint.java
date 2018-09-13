e67c04dc1d022cf7386d7726276df9cc2a90ffdf
武扬 <956826374@qq.com>
2018-09-13 10:42:16 +0800

    增加mypoint类并且增加测试这个类的函数

---
+++ src/unit10/TestMyPoint.java
@@ -0,0 +1,15 @@
+package unit10;
+
+/*
+测试MyPoint类
+(0,0)
+(10,30.5)
+显示它们的距离
+ */
+public class TestMyPoint {
+    public static void main(String[] args){
+        MyPoint p1 = new MyPoint();
+        MyPoint p2 = new MyPoint(10, 30.5);
+        System.out.println("两个点之间的距离是："+p1.distance(p2));
+    }
+}
