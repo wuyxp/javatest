2523d103e4e1fff81c140ea24fe976e5e6689994
武扬 <956826374@qq.com>
2018-09-27 15:27:10 +0800

    增加font对象

--- src/unit14/NodeStyleRotateDemo.java
+++ src/unit14/NodeStyleRotateDemo.java
@@ -3,17 +3,25 @@ package unit14;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class NodeStyleRotateDemo extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Label label = new Label("hello world");
         Button button = new Button("ook");
         StackPane pane = new StackPane();
         button.setStyle("-fx-border-color: blue; -fx-color: pink");
         button.setRotate(-45);
+        label.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 20));
+        label.setRotate(-45);
         pane.getChildren().add(button);
+        pane.getChildren().add(label);
         pane.setRotate(45);
         pane.setStyle("-fx-border-color:red; -fx-background-color: lightgray;");
 
