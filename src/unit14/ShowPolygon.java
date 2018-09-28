5e9b724934cb82aacd9b927924bbf774cf3755d4
武扬 <956826374@qq.com>
2018-09-28 16:22:33 +0800

    画不规则图形

---
+++ src/unit14/ShowPolygon.java
@@ -0,0 +1,37 @@
+package unit14;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class ShowPolygon extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        Polygon polygon = new Polygon();
+        pane.getChildren().add(polygon);
+        polygon.setFill(Color.WHITE);
+        polygon.setStroke(Color.BLACK);
+        ObservableList<Double> list = polygon.getPoints();
+
+        final double WIDTH = 200, HEIGHT = 200;
+        final int LENGTH = 5;
+        double centerX = WIDTH / 2;
+        double centerY = HEIGHT / 2;
+        double radius = Math.min(WIDTH, HEIGHT) * .4;
+
+
+        for (int i = 0; i < LENGTH; i++) {
+            list.add(centerX + radius * Math.cos(2 * i * Math.PI / LENGTH));
+            list.add(centerY + radius * Math.sin(2 * i * Math.PI / LENGTH));
+        }
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
