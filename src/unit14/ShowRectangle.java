c2c849a97029657e80fce057d46d35be432e7f30
武扬 <956826374@qq.com>
2018-09-28 15:19:59 +0800

    画矩形

---
+++ src/unit14/ShowRectangle.java
@@ -0,0 +1,47 @@
+package unit14;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class ShowRectangle extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+
+        Rectangle r1 = new Rectangle(25, 10, 60, 30);
+        r1.setStroke(Color.BLACK);
+        r1.setFill(Color.WHITE);
+        pane.getChildren().add(new Text(10, 27, "r1"));
+        pane.getChildren().add(r1);
+
+        Rectangle r2 = new Rectangle(25, 50, 60, 30);
+        pane.getChildren().add(new Text(10, 67, "r2"));
+        pane.getChildren().add(r2);
+
+        Rectangle r3 = new Rectangle(25, 90, 60, 30);
+        r3.setArcWidth(10);
+        r3.setArcHeight(50);
+        pane.getChildren().add(new Text(10, 107, "r3"));
+        pane.getChildren().add(r3);
+
+        for (int i = 0; i < 4; i++) {
+            Rectangle r = new Rectangle(100, 50, 100, 30);
+            r.setRotate(i * 360 / 8);
+            r.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
+            r.setFill(Color.WHITE);
+            pane.getChildren().add(r);
+        }
+
+        Scene scene = new Scene(pane, 250, 150);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
